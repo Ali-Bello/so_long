74959c7efa434df1f89bab2804531ebaeea5ce16
Ali Derraj <aderraj@c4r7p7.1337.ma>
2024-06-29 01:08:13 +0100

    player bg error

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: aderraj <aderraj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 02:05:17 by aderraj           #+#    #+#             */
-/*   Updated: 2024/06/29 00:09:36 by aderraj          ###   ########.fr       */
+/*   Updated: 2024/06/29 01:07:48 by aderraj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ void	get_player_mapxy(t_game *game)
 	game->player->y = i;
 	game->player->x_px = j * IMG_WIDTH;
 	game->player->y_px = i * IMG_HEIGHT;
-	game->player->init_x = game->player->x_px;
-	game->player->init_y = game->player->y_px;
 	game->player->target_x = game->player->x_px;
 	game->player->target_y = game->player->y_px;
 }
@@ -56,11 +54,27 @@ void set_positions(t_game *game, int new_x, int new_y)
     game->player->x = new_x;
     game->player->y = new_y;
 }
-
+void	draw_background(t_game *game)
+{
+	if (game->map[game->player->y][game->player->x] == '0')
+		ft_cpy_img(game->assests[0]->animations->frames->img,
+				game->bg, game->player->x * IMG_WIDTH, game->player->y * IMG_HEIGHT);
+	if (game->map[game->player->y - 1][game->player->x] == '0')
+		ft_cpy_img(game->assests[0]->animations->frames->img,
+				game->bg, game->player->x * IMG_WIDTH, (game->player->y - 1) * IMG_HEIGHT);
+	if (game->map[game->player->y + 1][game->player->x] == '0')
+		ft_cpy_img(game->assests[0]->animations->frames->img,
+				game->bg, game->player->x * IMG_WIDTH, (game->player->y + 1) * IMG_HEIGHT);
+	if (game->map[game->player->y][game->player->x - 1] == '0')
+		ft_cpy_img(game->assests[0]->animations->frames->img,
+				game->bg, (game->player->x - 1) * IMG_WIDTH, game->player->y * IMG_HEIGHT);
+	if (game->map[game->player->y][game->player->x + 1] == '0')
+		ft_cpy_img(game->assests[0]->animations->frames->img,
+				game->bg, (game->player->x + 1)* IMG_WIDTH, game->player->y * IMG_HEIGHT);
+}
 void	render_player(t_game *game)
 {
-    // draw background
-	ft_cpy_img(game->assests[0]->img, game->bg, game->player->init_x, game->player->init_y);
+	draw_background(game);
 	ft_cpy_img(game->assests[0]->img, game->bg, game->player->target_x, game->player->target_y);
     if (!game->player->current_frame)
         game->player->current_frame = game->player->current_animation->frames;
@@ -77,11 +91,7 @@ void	render_player(t_game *game)
 		game->player->y_px -= STEP_SIZE;
 	if (game->player->x_px == game->player->target_x
 		&& game->player->y_px == game->player->target_y)
-	{
 		game->player_dir = -1;
-		game->player->init_x = game->player->x_px;
-		game->player->init_y = game->player->y_px;
-	}
 }
 
 void	move_player(t_game *game, int new_x, int new_y)
